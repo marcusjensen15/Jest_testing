ff17176535f47eb0a0ad90256343f524f11d1fb2
Marcus Jensen <marcusjensen@Marcuss-MacBook-Pro.local>
2020-11-13 13:18:44 -0800

    third test passing

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -10,3 +10,8 @@ test('absolute - should return a postive number if input number is negative', ()
     const result = lib.absolute(-2);
     expect(result).toBe(2);
  });
+
+ test('absolute - should return zero if input is zero', () => {
+    const result = lib.absolute(0);
+    expect(result).toBe(0);
+ });
