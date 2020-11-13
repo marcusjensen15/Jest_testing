0f12049d81738dd921d798d711e6286212080bba
Marcus Jensen <marcusjensen@Marcuss-MacBook-Pro.local>
2020-11-13 13:37:29 -0800

    greeting test passing

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -1,17 +1,33 @@
 const { test } = require("@jest/globals");
 const lib = require('../lib');
 
-test('absolute - should return a postive number if input number is positive', () => {
-   const result = lib.absolute(2);
-   expect(result).toBe(2);
+describe('absolute', () => {
+
+
+    it('should return a postive number if input number is positive', () => {
+        const result = lib.absolute(2);
+        expect(result).toBe(2);
+     });
+     
+     it('should return a postive number if input number is negative', () => {
+         const result = lib.absolute(-2);
+         expect(result).toBe(2);
+      });
+     
+      it('should return zero if input is zero', () => {
+         const result = lib.absolute(0);
+         expect(result).toBe(0);
+      });
+
+
 });
 
-test('absolute - should return a postive number if input number is negative', () => {
-    const result = lib.absolute(-2);
-    expect(result).toBe(2);
- });
 
- test('absolute - should return zero if input is zero', () => {
-    const result = lib.absolute(0);
-    expect(result).toBe(0);
- });
+describe('greet', () => {
+
+    it('should return the greeting message', () => {
+       const result = lib.greet('Marcus');
+       expect(result).toBe('Welcome Marcus');
+
+    });
+});
