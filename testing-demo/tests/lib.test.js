71ab9b0b7f325b22b4b8b1932839546e4e8eb393
Marcus Jensen <marcusjensen@Marcuss-MacBook-Pro.local>
2020-11-14 11:26:35 -0800

    all tests still passing

--- testing-demo/tests/lib.test.js
+++ testing-demo/tests/lib.test.js
@@ -1,6 +1,7 @@
 const { test } = require("@jest/globals");
 const lib = require('../lib');
 const db = require('../db');
+const mail = require('../mail');
 
 describe('absolute', () => {
 
@@ -95,5 +96,21 @@ describe('applyDiscount', () => {
        expect(order.totalPrice).toBe(9);
 
 
+    });
+});
+
+describe('notifyCustomer', () => {
+
+    it('should send an email to the customer', () => {
+        db.getCustomerSync = function(customerId) {
+            return {email: 'a', };
+        }
+        let mailSent = false;
+
+        mail.send = function(email, message) {
+            mailSent = true;
+        }
+        lib.notifyCustomer({ customerId: 1});
+        expect(mailSent).toBe(true);
     });
 });
